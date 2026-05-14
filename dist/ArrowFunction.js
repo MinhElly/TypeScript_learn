a5d35a546fc51700462e675a93ef50e7294f8656
MinhElly <minhnguyen071006@gmail.com>
2026-05-14 08:51:52 +0700

    Add ArrowFunction

---
+++ dist/ArrowFunction.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const button = document.getElementById('btn');
+button?.addEventListener('click', () => {
+    console.log('Button clicked!');
+});
